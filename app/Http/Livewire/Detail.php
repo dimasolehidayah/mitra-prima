b6a1b22740f0a227720f04dfd6967f06e86bade6
dimasolehidayah <dimassolehhidayah@gmail.com>
2021-03-27 14:02:32 +0700

    revisi

--- app/Http/Livewire/Detail.php
+++ app/Http/Livewire/Detail.php
@@ -9,24 +9,27 @@ use App\Models\Setting;
 
 class Detail extends Component
 {
-    public $data;
-    public $foto;
-    public $fotolama;
-    public $id_kategori;
-    public $harga;
-    public $stok;
-    public $produkId, $setting;
+    public $data,$foto, $fotolama
+    , $id_kategori
+    , $harga
+    , $stok
+    , $produkId, $setting,$check;
 
     public function mount($id)
     {
-       $this->data = Produk::where('id', $id)->latest('produk.created_at')->leftJoin('kategori', 'kategori.id_kategori', '=', 'produk.id_kategori')->first();
+        $check = Produk::find($id);
+        if($check != null){
+            $this->data = Produk::where('id', $id)->latest('produk.created_at')->leftJoin('kategori', 'kategori.id_kategori', '=', 'produk.id_kategori')->first();
+        }
 
     }
     public function render()
     {
-        $kategori =  Kategori::orderBy('id_kategori','ASC')->first();
-        $this->setting = Setting::latest()->get();
+        if (empty($this->data)) {
+            $this->redirect('/notfound');
+        }
+            $this->setting = Setting::latest()->get();
 
-        return view('livewire.detail')->extends('layout.frontend', ['setting' => $this->setting]);
+            return view('livewire.detail')->extends('layout.frontend', ['setting' => $this->setting]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,13 +27,16 @@ use App\Http\Livewire\Index;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|   
+|
 */
 
 Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/notfound', function () {
+    return view('layout.pageEror');
+});
 Route::get('/index', Index::class);
 Route::get('/detail/{id}', Detail::class);
 
